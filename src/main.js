3da83e58232a6e54dbdec30ad00ec5c0cd02c9c6
vitoria-a <vitoriagomes@alu.ufc.br>
2022-05-12 13:42:47 -0300

    importing the Button component

--- src/main.js
+++ src/main.js
@@ -1,22 +1,21 @@
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
-import './assets/saas/styles.scss';
+import './assets/sass/styles.scss';
 
 import { createApp } from 'vue';
-
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import InputText from 'primevue/inputtext';
 import InputNumber from 'primevue/inputnumber';
-
-
+import Button from 'primevue/button';
 
 const app = createApp(App);
 app.use(PrimeVue);
 
 app.component('InputText', InputText);
 app.component('InputNumber', InputNumber);
+app.component('Button', Button);
 
 app.mount('#app');
 
