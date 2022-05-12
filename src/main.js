961f7e72c3f5b27b484f06168d3bdb9b4c482d45
vitoria-a <vitoriagomes@alu.ufc.br>
2022-05-12 13:09:20 -0300

    importing PrimeVue and its InputText and InputNumber components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import './assets/saas/styles.scss';
 
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import InputNumber from 'primevue/inputnumber';
+
+
+
+const app = createApp(App);
+app.use(PrimeVue);
+
+app.component('InputText', InputText);
+app.component('InputNumber', InputNumber);
+
+app.mount('#app');
+
