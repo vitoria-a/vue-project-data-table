3bc4e42372d568f1a672fe26fafc5d08d897fb26
vitoria-a <vitoriagomes@alu.ufc.br>
2022-05-12 13:15:59 -0300

    creating two input fields and a button to save the data. One of the inputs will be to add the product name and the other the price

--- src/components/ProductTable.vue
+++ src/components/ProductTable.vue
@@ -1,5 +1,21 @@
 <template>
-  <div></div>
+  <div>
+    <h3>Price Table</h3>
+    <div class="p-inputgroup">
+      <span class="p-inputgroup-addon">
+        <i class="pi pi-box"></i>
+      </span>
+      <InputText placeholder="Enter the product name" />
+    </div>
+
+    <div class="p-inputgroup">
+      <span class="p-inputgroup-addon">$</span>
+      <InputNumber placeholder="Enter the product price" />
+    </div>
+
+    <Button label="Adicionar" class="p-button-success" />
+    <br />
+  </div>
 </template>
 
 <script>
