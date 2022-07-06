08e06ce4ce9aad9aa77861cce62f77131fc53bc5
vitoria-a <vitoriagomes@alu.ufc.br>
2022-07-05 21:41:55 -0300

    Adjusting the getAllProducts service and implementing the getProductId, patchInactiveProductId and patchActiveProductId services.

--- src/services/productService.js
+++ src/services/productService.js
@@ -2,9 +2,13 @@ import axios from 'axios';
 
 const baseURL =  'http://localhost:8080/api/v1/products';
 
-export function getAllProducts() {
-    return axios.get(baseURL);
-};
+export function getAllProducts(isActive) {
+    return axios.get(baseURL + `?isActive=${isActive}`);
+}
+
+export function getProductId(id) {
+    return axios.get(baseURL + `/${id}`);
+}
 
 export function postProduct(product) {
     return axios.post(baseURL, product);
@@ -16,4 +20,12 @@ export function putProduct(id, product) {
 
 export function deleteProduct(id) {
     return axios.delete(baseURL + `/${id}`);
+}
+
+export function patchInactiveProductId(id) {
+    return axios.patch(baseURL + `/${id}/inactive`);
+}
+
+export function patchActiveProductId(id) {
+    return axios.patch(baseURL + `/${id}/active`);
 }
