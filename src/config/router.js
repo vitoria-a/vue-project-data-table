3125148a7dc4fa85d8760d9f869d35ddabfe2586
vitoria-a <vitoriagomes@alu.ufc.br>
2022-07-07 21:47:52 -0300

    Creating the Product Details page and adding it to the route.

--- src/config/router.js
+++ src/config/router.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import ProductManagement from '../pages/ProductManagement';
+import ProductDetails from '../pages/ProductDetails';
 
 const routes = [
   {
     path: '/',
     name: 'productManagement',
     component: ProductManagement
+  },
+  {
+    path: '/:id/details',
+    name: 'productDetails',
+    component: ProductDetails,
+    props: true
   }
 ];
 
