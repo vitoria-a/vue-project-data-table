cfdbf5924d806fb624bac29a39ad87dcb49256a3
vitoria-a <vitoriagomes@alu.ufc.br>
2022-07-07 22:20:04 -0300

    Importing the getProductId service on the Product Details page.

--- src/pages/ProductDetails.vue
+++ src/pages/ProductDetails.vue
@@ -1,9 +1,35 @@
 <template>
-  <h1> Details </h1>
+  <h3> Details </h3>
+  <p> {{ product.name }}</p>
 </template>
 
 <script>
-export default {
 
+import { getProductId } from '../services/productService.js'
+export default {
+    name: 'ProductDetails',
+    props: {
+        id: String,
+        default: ""
+    },
+    data() {
+        return {
+            product: {}
+        }
+    },
+    async mounted() {
+        await this.requestGetProductId(this.id);
+    },
+    methods: {
+    async requestGetProductId(productId) {
+      try {
+        const response = await getProductId(productId);
+        let data = response.data.data;
+        this.product = { ...data };  
+      } catch (error) {
+        this.notification('error', `${error.response.data.errors}`);
+      }
+    },
+    }
 }
 </script>
