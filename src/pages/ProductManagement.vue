2a4ebb4e00feb29d4997f24a2f2c06b6c452ce39
vitoria-a <vitoriagomes@alu.ufc.br>
2022-07-07 21:08:09 -0300

    Code refactoring, decoupling responsibilities from requests and organizing methods.

--- src/pages/ProductManagement.vue
+++ src/pages/ProductManagement.vue
@@ -34,30 +34,16 @@
           v-tooltip.bottom="'Enter the description'" 
         />
       </div>
-
       <div class="button">
         <Button
           class="p-button-success"
           label="Adicionar"
           :disabled="hasProduct"
-          @click="requestPostProduct"
+          @click="save"
         />
       </div>
     </div>
-    <div class="options">
-      <div class="p-inputgroup">
-        <InputText
-          placeholder="Search by product ID"
-          v-model.trim="product.id"
-          v-tooltip.bottom="'Enter the product ID'"
-        />
-        <Button
-          icon="pi pi-search"
-          class="p-button-warning"
-          :disabled="hasProductId"
-          @click="requestGetProductId(product.id)"
-        />
-      </div>
+    <div class="buttons">
       <div class="button">
         <Button
           class="p-button-warning"
@@ -77,21 +63,24 @@
     <ConfirmPopup group="editProduct">
       <template #message="slotProps">
         <div class="pop-up-edit">
-          <p>{{ slotProps.message.titulo }}</p>
+          <p>{{ slotProps.message.title }}</p>
           <p> Name:
             <InputText
+              class="input"
               placeholder="Product Name"
               v-model="modifiedProduct.name"
             />
           </p>
           <p> Description:
             <InputText
+              class="input"
               placeholder="Product Description"
               v-model="modifiedProduct.description"
             />
           </p>
           <p> Price:
             <InputNumber
+              class="input"
               placeholder="Product Price"
               v-model="modifiedProduct.price"
               :minFractionDigits="2"
@@ -104,14 +93,14 @@
     <ConfirmPopup group="deleteProduct">
       <template #message="slotProps">
         <div class="pop-up-delete">
-          <p>{{ slotProps.message.titulo }}</p>
+          <p>{{ slotProps.message.title }}</p>
         </div>
       </template>
     </ConfirmPopup>
-    <ConfirmPopup group="moreOptions">
+    <ConfirmPopup group="status">
       <template #message="slotProps">
         <div class="pop-up-more-options">
-          <p>{{ slotProps.message.titulo }}</p>
+          <p>{{ slotProps.message.title }}</p>
         </div>
       </template>
     </ConfirmPopup>
@@ -119,11 +108,12 @@
     <Toast />
 
     <ProductTable
+      ref="productTable"
       :products="products"
       :deleteProduct="deleteProductId"
       :editProduct="editProductId"
-      :moreOptions="moreOptions"
-      :productActive="productActive"
+      :activeProduct="activeProductId"
+      :inactiveProduct="inactiveProductId"
     />
   </div>
 </template>
@@ -131,6 +121,7 @@
 <script>
 
 import ProductTable from '../components/ProductTable.vue';
+
 import {
   getAllProducts,
   postProduct,
@@ -148,83 +139,131 @@ export default {
   },
   data() {
     return {
+      active: true,
       modifiedProduct: {
         id: null,
         name: "",
         description: "",
-        price: null
+        price: 0
       },
       product: {
         id: null,
         name: "",
         description: "",
-        price: null
+        price: 0
       },
-      products: [],
-      productActive: false,
+      products: []
     };
   },
   computed: {
     hasProduct() {
-      let exists = true;
-      if (this.product.name && this.product.description) {
-        exists = false;
-      }
-      return exists;
+      return (this.product.name && this.product.description) ? false : true;
     },
     hasProductId() {
       return this.product.id ? false : true;
-
     },
     status() {
-      return this.productActive ? "active" : "inactive";
+      return this.active ? "inactive" : "active";
     }
   },
   async mounted() {
-    await this.requestGetAllProducts(true);
+    await this.requestGetAllProducts(this.active);
   },
   methods: {
     notification(severity, detail) {
       this.$toast.add({ severity: severity, summary: '', detail: detail, life: 3000 });
     },
-    async requestGetAllProducts(isActive) {
-      try {
-        const response = await getAllProducts(isActive);
-        let data = response.data.data;
-        this.products = data;
-      } catch {
-        this.products = [];
+    clear() {
+      this.product = {
+        id: null,
+        name: "",
+        description: "",
+        price: 0
       }
     },
-    async requestPostProduct() {
-      try {
-        await postProduct(this.product);
-        this.notification('success', `${this.product.name} registered`);
-        this.requestGetAllProducts(true);
-        this.product.name = "";
-        this.product.description = "";
-        this.product.price = "";
-      } catch (error) {
-        this.notification('error', `${error.response.data.errors}`);
-      }
+    activeProducts() {
+      this.active = true;
+      this.$refs.productTable.active = false;
+      this.requestGetAllProducts(true);
     },
-    async requestPutProduct(productId, product) {
-      try {
-        await putProduct(productId, product);
-        this.notification('success', `${product.name} updated`);
-        this.requestGetAllProducts(true);
-      } catch (error) {
-        this.notification('error', `${error.response.data.errors}`);
-      }
+    inactiveProducts() {
+      this.active = false;
+      this.$refs.productTable.active = true;
+      this.requestGetAllProducts(false);
     },
-    async requestDeleteProduct(product) {
-      try {
-        await deleteProduct(product.id);
-        this.notification('success', `${product.name} deleted`);
-        this.requestGetAllProducts(false);
-      } catch (error) {
-        this.notification('error', `${error.response.data.errors}`);
-      }
+    editProductId(event, product) {
+      this.modifiedProduct = { ...product };
+      this.$confirm.require({
+        group: "editProduct",
+        target: event.currentTarget,
+        title: "Do you really want to edit?",
+        accept: () => {
+            this.edit(this.modifiedProduct);
+        },
+        reject: () => {
+          this.notification('info', `The product ${product.name} has not been updated`);
+        }
+      });
+    },
+    deleteProductId(event, product) {
+      this.$confirm.require({
+        group: "deleteProduct",
+        target: event.currentTarget,
+        title: `Do you really want to delete ${product.name}?`,
+        accept: () => {
+          this.delete(product);
+        },
+        reject: () => {
+          this.notification('info', `The product ${product.name} has not been deleted`);
+        }
+      });
+    },
+    activeProductId(event, product) {
+      this.$confirm.require({
+        group: "status",
+        target: event.currentTarget,
+        title: `Do you want to ${this.status} the product?`,
+        accept: () => {
+          this.activeProduct(product.id);
+        },
+        reject: () => {
+          this.notification('info', `No changes have been made`);
+        }
+      });
+    },
+    inactiveProductId(event, product) {
+      this.$confirm.require({
+        group: "status",
+        target: event.currentTarget,
+        title: `Do you want to ${this.status} the product?`,
+        accept: () => {
+          this.inactiveProduct(product.id);
+        },
+        reject: () => {
+          this.notification('info', `No changes have been made`);
+        }
+      });
+    },
+    async save() {
+      await this.requestPostProduct(this.product);
+      this.clear();
+      await this.requestGetAllProducts(this.active);
+    },
+    async edit(product) {
+      await this.requestPutProduct(product);
+      await this.requestGetAllProducts(this.active);
+    },
+    async delete(product) {
+      await this.requestDeleteProduct(product);
+      await this.requestGetAllProducts(this.active);
+    },
+    async inactiveProduct(productId) {
+      await this.requestPatchInactiveProductId(productId);
+      await this.requestGetAllProducts(this.active);
+    },
+    async activeProduct(productId) {
+      await this.requestPatchActiveProductId(productId);
+      await this.requestGetAllProducts(this.active);
     },
     async requestGetProductId(productId) {
       try {
@@ -238,10 +277,39 @@ export default {
         this.notification('error', `${error.response.data.errors}`);
       }
     },
-    async requestPatchInactiveProductId(productId) {
+    async requestGetAllProducts(isActive) {
       try {
-        await patchInactiveProductId(productId);
-        this.requestGetAllProducts(true);
+        const response = await getAllProducts(isActive);
+        let data = response.data.data;
+        this.products = data;
+      } catch {
+        this.products = [];
+      }
+    },
+    async requestPostProduct(product) {
+      try {
+        await postProduct(product);
+        this.notification('success', `${product.name} registered`);
+      } catch (error) {
+        this.notification('error', `${error.response.data.errors}`);
+      }
+    },
+    async requestPutProduct(product) {
+      try {
+        await putProduct(product.id, {
+          name: product.name,
+          description: product.description,
+          price: product.price
+        } );
+        this.notification('success', `${product.name} updated`);
+      } catch (error) {
+        this.notification('error', `${error.response.data.errors}`);
+      }
+    },
+    async requestDeleteProduct(product) {
+      try {
+        await deleteProduct(product.id);
+        this.notification('success', `${product.name} deleted`);
       } catch (error) {
         this.notification('error', `${error.response.data.errors}`);
       }
@@ -249,64 +317,16 @@ export default {
     async requestPatchActiveProductId(productId) {
       try {
         await patchActiveProductId(productId);
-        this.requestGetAllProducts(false);
       } catch (error) {
         this.notification('error', `${error.response.data.errors}`);
       }
     },
-    editProductId(event, product) {
-      this.modifiedProduct = { ...product };
-      this.$confirm.require({
-        group: "editProduct",
-        target: event.currentTarget,
-        titulo: "Do you really want to edit?",
-        accept: () => {
-            this.requestPutProduct(this.modifiedProduct.id, this.modifiedProduct);
-        },
-        reject: () => {
-          this.notification('info', `The product ${product.name} has not been updated`);
-        }
-      });
-    },
-    deleteProductId(event, product) {
-      this.$confirm.require({
-        group: "deleteProduct",
-        target: event.currentTarget,
-        titulo: `Do you really want to delete ${product.name}?`,
-        accept: () => {
-          this.requestDeleteProduct(product);
-        },
-        reject: () => {
-          this.notification('info', `The product ${product.name} has not been deleted`);
-        }
-      });
-    },
-    moreOptions(event, product) {
-      this.$confirm.require({
-        group: "moreOptions",
-        target: event.currentTarget,
-        titulo: `Do you want to ${this.status} the product?`,
-        accept: () => {
-          if (this.status === 'inactive') {
-            this.requestPatchInactiveProductId(product.id);
-            this.notification('success', `${product.name} inactivated`);
-          } else {
-            this.requestPatchActiveProductId(product.id);
-            this.notification('success', `${product.name} activated`);
-          }
-        },
-        reject: () => {
-          this.notification('info', `No changes have been made`);
-        }
-      });
-    },
-    activeProducts() {
-      this.productActive = false;
-      this.requestGetAllProducts(true);
-    },
-    inactiveProducts() {
-      this.productActive = true;
-      this.requestGetAllProducts(false);
+    async requestPatchInactiveProductId(productId) {
+      try {
+        await patchInactiveProductId(productId);
+      } catch (error) {
+        this.notification('error', `${error.response.data.errors}`);
+      }
     } 
   }
 }
